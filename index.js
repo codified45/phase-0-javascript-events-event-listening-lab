b99ca7c8bfeaf44ab0562d5065ce0d7ba64781a0
Jack Lach <jack45k@gmail.com>
2022-12-31 00:21:33 -0500

    commit tree 95d076bdc209d06a09ce69792b9670df3db2686e

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
+
+const input = document.getElementById('button');
+
+function clickAlert() {
+    alert('I was clicked!');
+  }
+
 function addingEventListener() {
+      input.addEventListener('click', clickAlert);
 }
+
+addingEventListener();
+
+
+// This is also ok but above is slightly better. Doesn't pass the learn test though because its looking for Adding not add
+
+// input.addEventListener('click', function() {
+//     alert('I was clicked');
+// });
